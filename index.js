9da916d33fcd1d72b6324dbbfeb2594b6f9eed6b
“Raizo-n” <sanpincha0826@gmail.com>
2023-01-15 03:53:41 -0800

    second commit

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-const { text } = require('express');
-const fs = require('fs');
-const http = require('http');
+const fs = require("fs");
+const http = require("http");
 const port = 8000;
-const path = require('path');
 const server = http.createServer();
 
-server.on('request', (req, res) => {
+server
+  .on("request", (req, res) => {
     // console.log(req);
-    if(req.url === '/'){
-        res.statusCode = 200;
-        res.setHeader = ('Content-Type', 'text/plain');
-        res.write(`
+    if (req.url === "/") {
+      res.statusCode = 200;
+      res.setHeader = ("Content-Type", "text/plain");
+      res.write(`
         <html>
                 <head>
                     <title>Send a message</title>
@@ -22,19 +21,11 @@ server.on('request', (req, res) => {
                 </body>
             </html>
         `);
-        res.end();
+      res.end();
     }
 
-    // const filePath = path.join(__filename, 'message.txt', req.url === '/' ? 'index.html' : req.url);
-    // const extName = path.extname(filePath);
-
-    // fs.readFile(filePath, (err, content) => {
-    //     res.writeHead(200, {'Content-Type': 'text/html'})
-    //     res.end(content, 'utf-8')
-    // })
-
-    if(req.url === '/write-message' && req.method === 'GET'){
-        res.write(`
+    if (req.url === "/write-message" && req.method === "GET") {
+      res.write(`
             <html>
                 <head>
                     <title>Send a message</title>
@@ -46,33 +37,33 @@ server.on('request', (req, res) => {
                     </form>
                 </body>
             </html>
-        `)
+        `);
     }
-    if(req.url === '/write-message' && req.method === 'POST'){
-        const body = [];
-        req.on('data', (chunk)=>{
-            body.push(chunk);
-        })
-        req.on('end', () => {
-            const parsedBody = Buffer.concat(body).toString();
-            const message = parsedBody.split('=')[1];
-            console.log(message);
-            fs.writeFile('message.txt', message, (err)=>{
-                if(err) throw err;
-                res.statusCode = 302;
-                res.setHeader('Location', '/');
-                return res.end();
-            })
-        })
-    }
-
-    if(req.url === '/read-message'){
-        res.statusCode = 200;
-        res.setHeader = ('Content-Type', 'text/plain');
-        // fs.readFile(path.join(__filename, 'message.txt'))
-        res.end(`<h1>${path.join(__filename, 'message.txt')}</h1>`);
+    if (req.url === "/write-message" && req.method === "POST") {
+      const body = [];
+      req.on("data", (chunk) => {
+        body.push(chunk);
+      });
+      req.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1];
+        console.log(message);
+        fs.writeFile("message.txt", message, (err) => {
+          if (err) throw err;
+          res.statusCode = 302;
+          res.setHeader("Location", "/");
+          return res.end();
+        });
+      });
     }
 
-}).listen(8000, () => console.log(`Listening on port ${port}`));
+    if (req.url === "/read-message") {
+      res.statusCode = 200;
+      res.setHeader = ("Content-Type", "text/plain");
 
-// Respond to requests from “/read-message” by sending some html that displays the content of your text file.
+      fs.readFile("./message.txt", "utf-8", (err, text) => {
+        if (err) throw err;
+        res.end(`<h1>${text}</h1>`);
+      });
+    }
+  }).listen(8000, () => console.log(`Listening on port ${port}`));
